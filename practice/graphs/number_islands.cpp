165323c43c89909e2e146511fa48deb3881edeeb
niiquaye <omarmon@uwaterloo.ca>
2025-07-26 20:45:06 -0400

    lc 200 dfs solution with lambdas

--- practice/graphs/number_islands.cpp
+++ practice/graphs/number_islands.cpp
@@ -12,9 +12,60 @@
 #include <queue>
 
 
+int solution_dfs(const std::vector<std::vector<char>>& grid)
+{
+    if (grid.empty() || grid[0].empty()) return 0;
+
+    int rows = grid.size();
+    int cols = grid[0].size();
+
+    std::vector<std::vector<bool>> visited (rows, std::vector<bool>(cols, false));
+
+    int islands {0};
+
+    auto check_bounds_and_not_visited = [&](int r, int c) -> bool {
+        if (r < rows && c < cols && r >= 0 && c >= 0 && visited[r][c] == false && grid[r][c] == '1')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    };
+
+
+    std::function<void(int, int)> dfs = [&](int r, int c) -> void 
+    {
+        if(!check_bounds_and_not_visited(r,c)) return;
+
+        visited[r][c] = true;
+
+        dfs(r+1, c);
+        dfs(r, c+1);
+        dfs(r-1, c);
+        dfs(r, c-1);
+
+
+    };
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            if (grid[r][c] == '1' && visited[r][c] == false)
+            {
+                islands++;
+                dfs(r,c);
+            }
+        }
+    }
+    return islands;
+}
+
 int solution_bfs(const std::vector<std::vector<char>>& grid)
 {
-    if (grid.empty()) return 0;
+    if (grid.empty() || grid[0].empty()) return 0;
 
     std::queue<std::pair<int, int>> queue {};
 
@@ -79,6 +130,7 @@ int solution_bfs(const std::vector<std::vector<char>>& grid)
 
 int main(int argc, char* argv[])
 {
+    std::cout << "BFS" << std::endl;
     std::cout << solution_bfs(
         {
             {'1','1','1','1','0'},
@@ -87,4 +139,15 @@ int main(int argc, char* argv[])
             {'0','0','0','0','0'}
         }
     ) << std::endl;
+
+    std::cout << "DFS" << std::endl;
+    std::cout << solution_dfs(
+        {
+            {'1','1','1','1','0'},
+            {'1','1','0','1','0'},
+            {'1','1','0','0','0'},
+            {'0','0','0','0','0'}
+        }
+    ) << std::endl;
+
 }
