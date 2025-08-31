4704e283f1b192be6d0d7864514167fded7f4aca
niiquaye <omarmon@uwaterloo.ca>
2025-08-31 01:01:28 -0400

    lc 787 solution

--- practice/graphs/cheapest_flights_k_stops.cpp
+++ practice/graphs/cheapest_flights_k_stops.cpp
@@ -12,11 +12,13 @@
 #include <queue>
 #include <map>
 #include <memory>
+#include <limits>
 
 typedef struct Flight
 {
     int node;
     int cost;
+    int stops = 0;
 }Flight;
 
 
@@ -35,28 +37,36 @@ int solution(int n, const std::vector<std::vector<int>>& flight, int src, int ds
 
     auto comparator = [](const Flight& a, const Flight& b) -> bool 
     {
-        return a.cost < b.cost;
+        return a.cost > b.cost;
     };
     std::priority_queue<Flight, std::vector<Flight>, decltype(comparator)> p_queue(comparator);
 
     auto djikstra = [&]() -> int 
     {
-        int stops = k;
-        p_queue.push({.node = src, .cost = 0});
+        std::vector<int> running_stops(n, std::numeric_limits<int>::max());
+        std::vector<int> running_cost(n, std::numeric_limits<int>::max());
 
-        while(!p_queue.empty() && stops--)
+        p_queue.push({.node = src, .cost = 0, .stops = 0});
+
+        while (!p_queue.empty())
         {
             auto top = p_queue.top();
             p_queue.pop();
 
             if (top.node == dst)
             {
-                return top.cost; // return running cost
+                return top.cost;
             }
+
+            if (top.stops > k) continue;
+            if ( top.cost >= running_cost[top.node]  && top.stops >= running_stops[top.node]) continue;
+
+            running_cost[top.node] = std::min(running_cost[top.node], top.cost);
+            running_stops[top.node] = std::min(running_stops[top.node], top.stops);
 
             for (const auto& nei : map[top.node])
             {
-                p_queue.push({.node = nei, .cost = nei.cost + top.cost});
+                p_queue.push({.node = nei.node, .cost = (top.cost + nei.cost), .stops = (top.stops + 1 ), });
             }
         }
         return -1;
